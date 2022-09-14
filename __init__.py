3cee047dda5470454ad695d7a47825cf33fea639
lucamanfroi <lucamanfroi@outlook.com.br>
2022-09-14 20:03:32 -0300

    First commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from .extensions import db, migrate
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    
+    return app
