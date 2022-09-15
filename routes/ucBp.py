e9e74a331d043850ff3d44d6675ce08e2dd8dfbd
lucamanfroi <lucamanfroi@outlook.com.br>
2022-09-15 10:24:09 -0300

    Update BD

--- routes/ucBp.py
+++ routes/ucBp.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from ..extensions import db
 from ..models.uc import Uc
 
@@ -7,4 +7,7 @@ ucBp = Blueprint('ucBp', __name__)
 
 @ucBp.route('/uc')
 def uc_list():
-    # db.create_all()
+    #    return "Teste"
+    #    db.create_all()
+    ucs_query = Uc.query.all()
+    return render_template('uc_list.html', ucs=ucs_query)
